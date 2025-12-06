c071d8f43b8d56ef11be2a249604ea45496f46ac
Yaakoub tair <yaakoubt05@gmail.com>
2025-12-06 16:20:58 +0100

    add : table clients

--- sql/bankly_v2.sql
+++ sql/bankly_v2.sql
@@ -1,4 +1,13 @@
 CREATE DATABASE IF NOT EXISTS bankly_v2;
-USE DATABASE bankly_v2;
+USE  bankly_v2;
+CREATE TABLE IF NOT EXISTS clients(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE ,
+    cin VARCHAR(50) UNIQUE NOT NULL,
+    telephone VARCHAR(50) UNIQUE ,
+    adress text,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP
+);
 
 
